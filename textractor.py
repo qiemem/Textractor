6795486d0730950f86b79e2dec64e9e041b487a2
Bryan Head <bryan.head@gmail.com>
2013-11-25 19:36:10 -0600

    Add pydoc for make_tuples

--- textractor.py
+++ textractor.py
@@ -1,6 +1,17 @@
 import fileinput
 
 def make_tuples(tuple_size, sentences):
+    """
+    Iterates through the words in the sentences, yielding them as tuples of
+    size tuple_size.
+    
+    >>> list(make_tuples(3, ['hurray for information extraction', 'text mining is super fun']))
+    [('hurray', 'for', 'information'),
+     ('for', 'information', 'extraction'),
+     ('text', 'mining', 'is'),
+     ('mining', 'is', 'super'),
+     ('is', 'super', 'fun')]
+    """
     for sentence in sentences:
         words = sentence.split()
         for i, w in enumerate(words[:-(tuple_size - 1)]):
