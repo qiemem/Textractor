Fix backward normalization

textractor.py
@@ -44,7 +44,7 @@ def enum_range(seq, start=0, stop=None, step=1):
     for i in xrange(start, stop, step):
         yield i, seq[i]
 
-def forward_prob(trans_probs, init_probs, emit_probs, sequence, normalize = True):
+def forward_prob(trans_probs, init_probs, emit_probs, sequence, normalize = False):
     """
     For each state at each time step, calculates the probability that the hmm
     would produce the given sequence of observations up to that time step and
@@ -92,7 +92,7 @@ def forward_prob(trans_probs, init_probs, emit_probs, sequence, normalize = True
         return time_state_prob
 
 
-def backward_prob(trans_probs, emit_probs, sequence, normalize = True):
+def backward_prob(trans_probs, emit_probs, sequence, normalizers = None):
     """
     For each state at each time step, calculates the probability that the
     given observations after that time step would occur given being in that
@@ -103,38 +103,23 @@ def backward_prob(trans_probs, emit_probs, sequence, normalize = True):
         observation as a list of lists (or list of dicts).
         emit_probs[state][observation]
     sequence - The sequence of observations over time. sequence[time] = obs
+    normalizers - If given, normalizes each value of the result matrix
+        so normed_beta[t,s] = beta[t, s] / normalizers[t+1:].prod()
 
     Returns a matrix beta representing the backward probabilities such that
-    beta[t, s] = P(seq[t+1],..., seq[T] | state[t] = s). If normalize is true, 
-    then the values of the beta[t,:] are normalized on 
-    P(seq[t + 1] | seq[0], ..., seq[t]) and those normalizing factors are 
-    returned along with the matrix.
-
-    FIXME: I think normalization factors may be off by one. I can't quite tell
-    how the normalization should affect beta[T,:].
-    See http://cs.brown.edu/courses/archive/2006-2007/cs195-5/lectures/lecture33.pdf
-    and http://courses.media.mit.edu/2010fall/mas622j/ProblemSets/ps4/tutorial.pdf
-
+    beta[t, s] = P(seq[t+1],..., seq[T] | state[t] = s).
     """
     n = len(trans_probs)
     states = range(n)
 
     time_state_prob = np.array([[0.0]*n for _ in sequence])
     time_state_prob[-1,:] = 1.0
-    if normalize:
-        normalizers = np.array([0.0]*len(sequence))
-        normalizers[-1] = time_state_prob[-1,:].sum()
-        time_state_prob[-1,:] /= normalizers[-1]
 
     for t, observed in enum_range(sequence, -1, 0, -1):
         time_state_prob[t-1,:] = trans_probs.dot(time_state_prob[t,:] * emit_probs[:,observed])
-        if normalize:
-            normalizers[t-1] = time_state_prob[t-1,:].sum()
-            time_state_prob[t-1,:] /= normalizers[t-1]
-    if normalize:
-        return time_state_prob, normalizers
-    else:
-        return time_state_prob
+        if normalizers != None:
+            time_state_prob[t-1,:] /= normalizers[t]
+    return time_state_prob
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Given a bunch of sentences, outputs feature vectors of the words')